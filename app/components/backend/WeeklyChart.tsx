6e3be70ba717846713c084039edbfdb9527803f6
Mas Kumis <wildanblitar@gmail.com>
2025-01-08 21:12:43 +0700

    Tabula Rasa

--- app/components/backend/WeeklyChart.tsx
+++ app/components/backend/WeeklyChart.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WeeklyChart = () => {
   return (
     <div className="bg-slate-800 px-8 py-4 rounded-lg">
-      <h3 className="text-xl font-semibold">Weekly Pass</h3>
+      <h3 className="text-xl font-semibold">zxczxczxc</h3>
       <div></div>
     </div>
   );
