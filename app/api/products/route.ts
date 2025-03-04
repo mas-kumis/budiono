e1498360fc10351041929aac916a9d27cf0ae4bb
Mas Kumis Dev <wildanblitar@gmail.com>
2025-03-04 16:59:53 +0700

    prisma create

---
+++ app/api/products/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany();
+    return NextResponse.json(products);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,41 @@
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-// export async function GET() {
-//   const prisma = new PrismaClient();
-//   const users = await prisma.user.findMany();
-//   return NextResponse.json(users);
-// }
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    if (!data.name || !data.email || !data.password) {
+      return NextResponse.json(
+        { error: "Please fill all fields" },
+        { status: 400 }
+      );
+    }
+    const isEmailExist = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+    if (isEmailExist) {
+      return NextResponse.json(
+        { error: "Email already used" },
+        { status: 400 }
+      );
+    }
+    const user = await prisma.user.create({ data });
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
