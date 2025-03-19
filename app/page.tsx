b687709865266c7fa7be811bdfaf7b2123a53b20
Mas Kumis Dev <wildanblitar@gmail.com>
2025-03-19 16:47:38 +0700

    bun

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,3 @@
-import Headers from "./components/Headers";
-import Navbar from "./components/Navbar";
-import Popular from "./components/Popular";
-import Create from "./components/Create";
-import Product from "./components/Product";
-
 export default function Home() {
-  return (
-    <div>
-      <div
-        className="h-screen md:h-[600px] w-full  bg-cover bg-center"
-        style={{ backgroundImage: 'url("/headers.png")' }}
-      >
-        <Navbar />
-        <Headers />
-      </div>
-      <Popular />
-      <Create />
-      <Product />
-    </div>
-  );
+  return <div>Headers</div>;
 }
