f003d8b37ce04b7e490dbd3c3d61c2362d8101f3
Mas Kumis Dev <wildanblitar@gmail.com>
2025-03-23 11:43:18 +0700

    credentials github

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,24 +1,6 @@
 import NextAuth from "next-auth";
-// import GitHub from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
-import { saltAndHashPassword } from "@/utils/password";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        let user = null;
-        const pwHash = saltAndHashPassword(credentials.password);
-        user = await (credentials.email, pwHash);
-        if (!user) {
-          throw new Error("No user found");
-        }
-        return user;
-      },
-    }),
-  ],
+  providers: [Github] 
 });
