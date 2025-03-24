6d3af13210d49ed42dabef15669115bdcb3add0f
Mas Kumis Dev <wildanblitar@gmail.com>
2025-03-24 10:59:02 +0700

    credentials and google

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const page = () => {
   return (
-    <div>
-      <div className="grid grid-cols-4 gap-4 mx-4 my-2">
+    <div className="mx-8 my-8">
+      <div className="grid grid-cols-4 gap-4">
         {[1, 2, 3, 4].map((i) => (
           <Card key={i}>
             <CardHeader>
@@ -20,6 +27,31 @@ const page = () => {
             </CardHeader>
           </Card>
         ))}
+      </div>
+      <div className="my-4">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">UserId</TableHead>
+              <TableHead>Full Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Password</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,17 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
+import authConfig from "@/lib/auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  session: {
+    strategy: "jwt",
+  },
+  ...authConfig
 });
