3147cb1f3ae33d33ec49cf8c697668737fc1ac00
Mas Kumis Dev <wildanblitar@gmail.com>
2025-04-15 14:51:33 +0700

    rotate

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import * as motion from "motion/react-client";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+      <motion.div
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1.2, transition: { duration: 2 } }}
+      >
+        <motion.div
+          initial={{ rotate: 9 }}
+          className="border p-4 border-slate-500 rounded-xl"
+        >
+          <h1 className="text-xl font-bold">Welcome to the App</h1>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 }
