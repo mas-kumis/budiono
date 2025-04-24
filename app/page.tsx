56297942c6936103d7c52b57d09650391ee173ab
Mas Kumis Dev <wildanblitar@gmail.com>
2025-04-24 21:43:17 +0700

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import About from "@/components/layout/About";
 import Featured from "@/components/layout/Featured";
 import HelpSection from "@/components/layout/HelpSection";
 import FeaturedPropertyCarousel from "@/components/layout/FeaturedItemCarousel";
-import PropertyLocations from "@/components/layout/PropertyLocation";
 
 export default function Home() {
   return (
@@ -19,7 +18,6 @@ export default function Home() {
       <Featured />
       <HelpSection />
       <FeaturedPropertyCarousel />
-      <PropertyLocations />
       <div className="h-[1000px]"></div>
     </div>
   );
